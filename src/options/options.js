8991499cecf406b093560c4f2b1d7e37901e9044
Ankush Gupta <ankushgupta@twitter.com>
2014-06-22 00:26:45 -0700

    added bitcoin tipping options

--- src/options/options.js
+++ src/options/options.js
@@ -1,42 +1,43 @@
-var ref = new Firebase("https://mmfvc.firebaseio.com");
-var authClient = new FirebaseAuthClient(ref, function(error, user) {
-  if (error) {
-    alert(error);
-    return;
-  }
-  if (user) {
-    // User is already logged in.
-    doLogin(user);
+$('#amount').on('change', function() {
+  var match = $('#amount').val().match(/[0-9]*(\.[0-9]*)+/g)
+  if (match == null || match.length != 1 || match[0] != $('#amount').val()) {
+    $('#amount').attr('style', 'background-color:#ff6666;');
   } else {
-    // User is logged out.
-    showLoginBox();
+    $('#amount').attr('style', '');
+    chrome.storage.sync.set({'default_value': $('#amount').val()}, function() {});
   }
 });
+chrome.storage.sync.get('default_value', function(value) {
+  if (value['default_value'] != undefined)
+    $('#amount').val(value['default_value']);
+  else
+    $('#amount').val('0.001');
+});
 
-function showLoginBox() {
-  ...
-  // Do whatever DOM operations you need to show the login/registration box.
-  $("#registerButton").on("click", function() {
-    var email = $("#email").val();
-    var password = $("#password").val();
-    authClient.createUser(email, password, function(error,  user) {
-      if (!error) {
-        doLogin(user);
-      } else {
-        alert(error);
-      }
-    });
-  });
-}
+$('#currency').on('change', function() {
+  chrome.storage.sync.set({'default_currency': $('#currency').val()}, function() {});
+});
+chrome.storage.sync.get('default_currency', function(value) {
+  if (value['default_currency'] != undefined)
+    $('#' + value['default_currency']).attr('selected', true);
+});
 
-function showLoginBox() {
-  ...
-  // Do whatever DOM operations you need to show the login/registration box.
-  $("#loginButton").on("click", function() {
-    authClient.login("password", {
-      email: $("#email").val(),
-      password: $("#password").val(),
-      rememberMe: $("#rememberCheckbox").val()
-    });
-  });
-}
+$('#comment').on('change', function() {
+  chrome.storage.sync.set({'post_comment': $('#comment').is(':checked')}, function() {});
+});
+chrome.storage.sync.get('post_comment', function(value) {
+  if (value['post_comment'] != undefined)
+    $('#comment').prop('checked', value['post_comment']);
+  else
+    $('#comment').prop('checked', true);
+});
+
+$('#anonymous').on('change', function() {
+  chrome.storage.sync.set({'anonymous_send': !$('#anonymous').is(':checked')}, function() {});
+});
+chrome.storage.sync.get('anonymous_send', function(value) {
+  if (value['anonymous_send'] != undefined)
+    $('#anonymous').prop('checked', !value['anonymous_send']);
+  else
+    $('#anomymous').prop('checked', true);
+});
