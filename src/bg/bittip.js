79bb81102ed47e47faaeac2c2a7250f57220f10f
Ankush Gupta <ankushgupta@twitter.com>
2014-06-22 08:51:49 -0700

    lots of code cleanup for bittip 😊

--- src/bg/bittip.js
+++ src/bg/bittip.js
@@ -7,18 +7,20 @@ var checkCoinbaseLogin = function(success_callback, failure_callback) {
 		coinbase_secret = token['api_secret'];
 	});
 
+  chrome.storage.sync.get("amount", function(token) {
+    sendAmount = token['amount'];
+  });
+
+  chrome.storage.sync.get("default_currency", function(token) {
+    sendCurrency = token['default_currency'];
+  });
+
 	if (coinbase_secret != null && coinbase_api_key != null) {
 		return success_callback();
 	} else {
 		return failure_callback();
 	}
 };
-
-var fromUser;
-
-// The amount to send
-var sendAmount = 0.50;
-var sendCurrency = 'USD';
 
 var sendFailed = function(msg) {
 	console.log(msg)
@@ -28,49 +30,38 @@ var sendSuccess = function(msg) {
 	console.log(msg);
 }
 
-var sendBitcoin = function(destination_address, sendAmount, sendCurrency) {
-	// Sends money using Coinbase
+chrome.extension.onMessage.addListener(
+  function (message, sender, sendResponse) {
+    // Check to make sure the message contains the fields we want
+    if (message.bittip && message.targetEmail) {
+      console.log("whoa, just got a message!");
+      // Your method here
+      return true;
+    } else if (sendResponse) {
+      sendResponse();
+    }
+  }
+);
+
+var sendBitcoin = function(destination_address, note) {
+
 	var sendMoney = function(success_callback, failure_callback) {
     var nonce = (new Date()).getTime();
     // var signature;
     var url = "https://coinbase.com/api/v1/transactions/send_money"
     + "?transaction%5Bto%5D=" + destination_address
     + "&transaction%5Bamount_string%5D=" + sendAmount
-    + "&transaction%5Bamount_currency_iso%5D=" + sendCurrency;
-    var params = {
-        transaction: {
-          to: destination_address,
-          amount_string: sendAmount,
-          amount_currency_iso: sendCurrency
-        }
-      };
-    params = http_build_query(json_decode(params), true);
-    var message = "" + nonce + url;
-    console.log(message);
+    + "&transaction%5Bamount_currency_iso%5D=" + sendCurrency
+    + "&transaction%5Bnotes%5D=" + encodeURIComponent(note)
+    + "&transaction%5Breferrer_id%5D=52b1dfa7296f323fe50000be";
 
 		$.ajax(url, {
 			type: "POST",
-			// data: {
-			// 	transaction: {
-			// 		to: destination_address,
-			// 		amount_string: sendAmount,
-			// 		amount_currency_iso: sendCurrency
-			// 	}
-			// },
-      headers: {
-        ACCESS_KEY: coinbase_api_key,
-        ACCESS_SIGNATURE:  CryptoJS.HmacSHA256(message, coinbase_secret),
-        ACCESS_NONCE: nonce,
-      },
-
-
-
-
-      // beforeSend: function(jqXHR, settings){
-      //   var message = "" + nonce + url + this.data;
-      //   signature = CryptoJS.HmacSHA256(message, coinbase_secret);
-      //   jqXHR.setRequestHeader("ACCESS_SIGNATURE", signature);
-      // },
+			headers: {
+				ACCESS_KEY: coinbase_api_key,
+				ACCESS_SIGNATURE:  CryptoJS.HmacSHA256(nonce.toString() + url, coinbase_secret),
+				ACCESS_NONCE: nonce,
+			},
 			success: function(response, textStatus, jqXHR) {
 				if (response.success == true)
 					success_callback('sent ' + sendAmount + ' ' + sendCurrency + '...');
@@ -94,28 +85,17 @@ var sendBitcoin = function(destination_address, sendAmount, sendCurrency) {
 		});
 	};
 
-	// Now string it all together...
-	var postAuth = function() {
-		sendMoney(function() {
-				sendSuccess('success!');
-			}, function(msg) {
-				// Failed to send (not enough funds, etc)
-				sendFailed(msg);
-		});
-	}
-
+  // Check if logged in
 	checkCoinbaseLogin(function() {
 			// Already logged in...
-			postAuth();
+			sendMoney(function() {
+        sendSuccess('success!');
+      }, function(msg) {
+        // Failed to send (not enough funds, etc)
+        sendFailed(msg);
+      });
 		}, function() {
-			// Not authed, lets try to log in...
-			coinbaseLogin(function() {
-					// Authed now...
-					postAuth();
-				}, function(msg) {
-					// Failed to get user to login to their Coinbase acct...
-					sendFailed(msg);
-			});
+			// Not authed
       alert("Please configure the extension with your Coinbase credentials!");
 	});
 };
--- src/options/options.js
+++ src/options/options.js
@@ -1,65 +1,3 @@
-// $('#api_key').on('change', function() {
-//     chrome.storage.sync.set({'api_key': $('#api_key').val()}, function() {});
-//   }
-// );
-// chrome.storage.sync.get('api_key', function(value) {
-//   if (value['api_key'] != undefined)
-//     $('#api_key').val(value['api_key']);
-// });
-
-// $('#api_secret').on('change', function() {
-//     chrome.storage.sync.set({'api_secret': $('#api_secret').val()}, function() {});
-//   }
-// );
-// chrome.storage.sync.get('api_secret', function(value) {
-//   if (value['api_secret'] != undefined)
-//     $('#api_secret').val(value['api_secret']);
-// });
-
-// $('#amount').on('change', function() {
-//   var match = $('#amount').val().match(/[0-9]*(\.[0-9]*)+/g)
-//   if (match == null || match.length != 1 || match[0] != $('#amount').val()) {
-//     $('#amount').attr('style', 'background-color:#ff6666;');
-//   } else {
-//     $('#amount').attr('style', '');
-//     chrome.storage.sync.set({'default_value': $('#amount').val()}, function() {});
-//   }
-// });
-// chrome.storage.sync.get('default_value', function(value) {
-//   if (value['default_value'] != undefined)
-//     $('#amount').val(value['default_value']);
-//   else
-//     $('#amount').val('0.001');
-// });
-
-// $('#currency').on('change', function() {
-//   chrome.storage.sync.set({'default_currency': $('#currency').val()}, function() {});
-// });
-// chrome.storage.sync.get('default_currency', function(value) {
-//   if (value['default_currency'] != undefined)
-//     $('#' + value['default_currency']).attr('selected', true);
-// });
-
-// $('#comment').on('change', function() {
-//   chrome.storage.sync.set({'post_comment': $('#comment').is(':checked')}, function() {});
-// });
-// chrome.storage.sync.get('post_comment', function(value) {
-//   if (value['post_comment'] != undefined)
-//     $('#comment').prop('checked', value['post_comment']);
-//   else
-//     $('#comment').prop('checked', true);
-// });
-
-// $('#anonymous').on('change', function() {
-//   chrome.storage.sync.set({'anonymous_send': !$('#anonymous').is(':checked')}, function() {});
-// });
-// chrome.storage.sync.get('anonymous_send', function(value) {
-//   if (value['anonymous_send'] != undefined)
-//     $('#anonymous').prop('checked', !value['anonymous_send']);
-//   else
-//     $('#anomymous').prop('checked', true);
-// });
-
 function save_bittip_options() {
      var amt = document.getElementById('amount').value;
      var currency = document.getElementById('currency').value;
@@ -75,7 +13,6 @@ function save_bittip_options() {
 }
 
 function restore_bittip_options() {
-  // Use default value color = 'red' and likesColor = true.
   chrome.storage.sync.get({
     amount: '0.50',
     default_currency : 'USD',
