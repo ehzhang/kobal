a910520ad61d9bf6b9a047691aa022baf2039c55
Anubhav Jain <anubhav94@gmail.com>
2014-06-22 08:09:28 -0700

    Fixing options local storage persistence and removing checkboxes

--- src/options/options.js
+++ src/options/options.js
@@ -63,16 +63,12 @@
 function save_bittip_options() {
      var amt = document.getElementById('amount').value;
      var currency = document.getElementById('currency').value;
-     var comment = $('#comment').is(':checked');
-     var anonymous = $('#anonymous').is(':checked');
      var user_api_key = document.getElementById('api_key').value;
      var user_api_secret = document.getElementById('api_secret').value;
 
      chrome.storage.sync.set( {
         amount : amt,
         default_currency : currency,
-        post_comment : comment,
-        anonymous_send: anonymous,
         api_key: user_api_key,
         api_secret : user_api_secret
      });
@@ -81,15 +77,16 @@ function save_bittip_options() {
 function restore_bittip_options() {
   // Use default value color = 'red' and likesColor = true.
   chrome.storage.sync.get({
-    amt: '0.005',
+    amount: '0.50',
+    default_currency : 'USD',
+    api_key : null,
+    api_secret : null,
 
   }, function(items) {
-    document.getElementById('amount').value = items.amt;
+    document.getElementById('amount').value = items.amount;
     document.getElementById('currency').value = items.default_currency;
-    document.getElementById('comment').prop('checked', items.post_comment);
-    document.getElementById('anonymous').prop('checked', items.anonymous_send);
-    document.getElementById('api_key').value = items.user_api_key;
-    document.getElementById('api_secret').value = items.user_api_secret;
+    document.getElementById('api_key').value = items.api_key;
+    document.getElementById('api_secret').value = items.api_secret;
   });
 }
 
